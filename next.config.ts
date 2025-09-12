110c23960ceedf75fad108b29a7d785a60daff1b
Amdadul-haq <amdadulhaq3746@gmail.com>
2025-09-13 02:06:30 +0600

    fixing the pdf generation failed for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default nextConfig;
+
